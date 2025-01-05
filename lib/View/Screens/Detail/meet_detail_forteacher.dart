382f64b5e77b93602e96e90b3521d861117f8742
nurullahkilic <75576033+nurullahkilic@users.noreply.github.com>
2025-01-05 17:08:35 +0300

    Meet Detail Screns changed.

--- lib/View/Screens/Detail/meet_detail_forteacher.dart
+++ lib/View/Screens/Detail/meet_detail_forteacher.dart
@@ -1,4 +1,10 @@
+import 'package:education/Core/Constants/app_colors.dart';
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:education/View/Screens/Core/Student/student_profile_screen.dart';
 
 class MeetDetailForteacher extends StatefulWidget {
   final Map<String, dynamic> userData;
@@ -9,15 +15,317 @@ class MeetDetailForteacher extends StatefulWidget {
 }
 
 class _MeetDetailForteacherState extends State<MeetDetailForteacher> {
-
   @override
   void initState() {
     super.initState();
   }
+
+  // Method to launch the URL
+  Future<void> _launchURL(String url) async {
+    final Uri uri = Uri.parse(url);
+    if (await canLaunch(uri.toString())) {
+      await launch(uri.toString());
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  // Check if the current time is within the lesson's time range
+  bool _isWithinLessonTime(DateTime startTime, DateTime endTime) {
+    DateTime now = DateTime.now();
+    return now.isAfter(startTime) && now.isBefore(endTime);
+  }
+
   @override
   Widget build(BuildContext context) {
+    var lesson = widget.userData;
+    DateTime startTime = lesson['startTime'].toDate();
+    DateTime endTime = lesson['endTime'].toDate();
+
+    bool canJoinLesson = _isWithinLessonTime(startTime, endTime);
+
     return Scaffold(
-      appBar: AppBar(),
+      backgroundColor: AppColors.primary,
+      appBar: AppBar(
+        title: Text(
+          lesson['title'],
+          style: GoogleFonts.inter(fontWeight: FontWeight.bold, fontSize: 24),
+        ),
+        backgroundColor: Colors.transparent,
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 14),
+              child: Row(
+                children: [
+                  Container(
+                    height: 55,
+                    width: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.purple.shade50,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.calendar_month_outlined,
+                        color: Colors.purple.shade400,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Tarih',
+                          style: GoogleFonts.inter(
+                            color: AppColors.subTitleText,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Row(
+                          children: [
+                            Text(
+                              DateFormat('dd MMMM yyyy').format(startTime),
+                              style: GoogleFonts.inter(
+                                color: AppColors.headTitleText,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 12),
+                            Text(
+                              DateFormat('HH:mm').format(startTime),
+                              style: GoogleFonts.inter(
+                                color: AppColors.headTitleText,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                            SizedBox(width: 2),
+                            Text('-'),
+                            SizedBox(width: 2),
+                            Text(
+                              DateFormat('HH:mm').format(endTime),
+                              style: GoogleFonts.inter(
+                                color: AppColors.headTitleText,
+                                fontWeight: FontWeight.w500,
+                                fontSize: 16,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(top: 14),
+              child: Row(
+                children: [
+                  Container(
+                    height: 55,
+                    width: 55,
+                    decoration: BoxDecoration(
+                      color: Colors.yellow.shade50,
+                      borderRadius: BorderRadius.circular(12),
+                    ),
+                    child: Center(
+                      child: Icon(
+                        Icons.link,
+                        color: Colors.yellow.shade400,
+                      ),
+                    ),
+                  ),
+                  SizedBox(width: 24),
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Link',
+                          style: GoogleFonts.inter(
+                            color: AppColors.subTitleText,
+                            fontWeight: FontWeight.w500,
+                            fontSize: 14,
+                          ),
+                        ),
+                        Text(
+                          lesson['googleMeetLink'] ?? 'Bilgi Yok',
+                          style: TextStyle(
+                            fontSize: 15,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+            SizedBox(height: 22),
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  GestureDetector(
+                    onTap: canJoinLesson
+                        ? () {
+                            _launchURL(lesson['googleMeetLink']);
+                          }
+                        : null,
+                    child: Container(
+                      width: MediaQuery.of(context).size.width,
+                      height: 40,
+                      decoration: BoxDecoration(
+                        color: canJoinLesson
+                            ? AppColors.button
+                            : Colors.grey.shade300,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.queue_play_next_sharp,
+                            color: canJoinLesson
+                                ? Colors.white
+                                : Colors.grey.shade500,
+                          ),
+                          SizedBox(width: 12),
+                          Text(
+                            'Derse Git',
+                            style: GoogleFonts.inter(
+                              color: canJoinLesson
+                                  ? AppColors.primary
+                                  : Colors.grey.shade500,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              letterSpacing: -1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                  if (!canJoinLesson)
+                    Center(
+                      child: Padding(
+                        padding: const EdgeInsets.only(top: 8),
+                        child: Text(
+                          'Zamanı geldiğinde buton tıklanılabilir olacak',
+                          style: TextStyle(
+                            fontSize: 12,
+                            color: Colors.grey.shade500,
+                            fontWeight: FontWeight.w500,
+                          ),
+                        ),
+                      ),
+                    ),
+                ],
+              ),
+            ),
+            SizedBox(height: 22),
+            buildSectionTitle('Katılımcılar'),
+            SizedBox(height: 16),
+            StreamBuilder<DocumentSnapshot>(
+              stream: FirebaseFirestore.instance
+                  .collection('lessons')
+                  .doc(lesson['id'])
+                  .snapshots(),
+              builder: (context, snapshot) {
+                if (snapshot.hasError) {
+                  return Text('Bir hata oluştu');
+                }
+
+                if (!snapshot.hasData || !snapshot.data!.exists) {
+                  return Text('Henüz katılımcı bulunmamaktadır');
+                }
+
+                final data = snapshot.data!.data() as Map<String, dynamic>;
+
+                List<dynamic> participants = data['dersi_alanlar'] ?? [];
+
+                if (participants.isEmpty) {
+                  return Text('Henüz katılımcı bulunmamaktadır');
+                }
+
+                return ListView.builder(
+                  shrinkWrap: true,
+                  physics: NeverScrollableScrollPhysics(),
+                  itemCount: participants.length,
+                  itemBuilder: (context, index) {
+                    return GestureDetector(
+                      onTap: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => Scaffold(
+                              body: StudentProfileScreen(
+                                hideIcon: true,
+                                userId: participants[index]['userId'],
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                      child: Padding(
+                        padding: const EdgeInsets.symmetric(vertical: 8.0),
+                        child: Container(
+                          padding: EdgeInsets.only(left: 12),
+                          height: 60,
+                          decoration: BoxDecoration(
+                            color: AppColors.secondary,
+                            borderRadius: BorderRadius.circular(12),
+                          ),
+                          child: Row(
+                            children: [
+                              Icon(
+                                Icons.person,
+                                color: AppColors.button,
+                                size: 38,
+                              ),
+                              SizedBox(width: 12),
+                              Text(
+                                participants[index]['fullName'] ??
+                                    'İsimsiz Katılımcı',
+                                style: GoogleFonts.inter(
+                                  fontSize: 16,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                );
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
+}
+
+Widget buildSectionTitle(String title) {
+  return Text(
+    title,
+    style: GoogleFonts.inter(
+      color: AppColors.headTitleText,
+      fontWeight: FontWeight.bold,
+      fontSize: 24,
+    ),
+  );
 }
--- lib/View/Screens/Detail/meet_detail_screen.dart
+++ lib/View/Screens/Detail/meet_detail_screen.dart
@@ -1,17 +1,23 @@
+// ignore_for_file: deprecated_member_use
+
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:education/Core/Constants/app_colors.dart';
 import 'package:education/Data/Models/lesson.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
-import 'package:intl/intl.dart';  // intl paketini ekleyin
+import 'package:intl/intl.dart';
+import 'package:url_launcher/url_launcher.dart';
+import 'package:flutter/services.dart';
 
 enum LessonInfoType { tarih, ders, link }
 
 class MeetDetailScreen extends StatefulWidget {
   final Lesson lesson;
+  final bool isLessonTaken;
 
-  const MeetDetailScreen({super.key, required this.lesson});
+  const MeetDetailScreen(
+      {super.key, required this.lesson, required this.isLessonTaken});
 
   @override
   State<MeetDetailScreen> createState() => _MeetDetailScreenState();
@@ -24,36 +30,45 @@ class _MeetDetailScreenState extends State<MeetDetailScreen> {
       backgroundColor: AppColors.primary,
       appBar: AppBar(
         backgroundColor: AppColors.primary,
+        title: widget.isLessonTaken ? Text('') : Text(widget.lesson.title),
         actions: [
-          Padding(
-            padding: const EdgeInsets.only(right: 16),
-            child: Container(
-              width: 169,
-              height: 36,
-              decoration: BoxDecoration(
-                color: AppColors.button,
-                borderRadius: BorderRadius.circular(12),
-              ),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  Icon(
-                    Icons.queue_play_next_sharp,
-                    color: Colors.white,
+          widget.isLessonTaken
+              ? Padding(
+                  padding: const EdgeInsets.only(right: 16),
+                  child: GestureDetector(
+                    onTap: () {
+                      _launchURL(widget.lesson.googleMeetLink);
+                    },
+                    child: Container(
+                      width: 169,
+                      height: 36,
+                      decoration: BoxDecoration(
+                        color: AppColors.button,
+                        borderRadius: BorderRadius.circular(12),
+                      ),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(
+                            Icons.queue_play_next_sharp,
+                            color: Colors.white,
+                          ),
+                          SizedBox(width: 12),
+                          Text(
+                            'Derse Git',
+                            style: GoogleFonts.inter(
+                              color: AppColors.primary,
+                              fontWeight: FontWeight.bold,
+                              fontSize: 18,
+                              letterSpacing: -1,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
                   ),
-                  SizedBox(width: 12),
-                  Text(
-                    'Derse Git',
-                    style: GoogleFonts.inter(
-                        color: AppColors.primary,
-                        fontWeight: FontWeight.bold,
-                        fontSize: 18,
-                        letterSpacing: -1),
-                  ),
-                ],
-              ),
-            ),
-          ),
+                )
+              : SizedBox.shrink(),
         ],
       ),
       body: SingleChildScrollView(
@@ -64,7 +79,7 @@ class _MeetDetailScreenState extends State<MeetDetailScreen> {
             children: [
               lessonsInfo(
                 'Tarih',
-                formatTimestamp(widget.lesson.startTime), 
+                formatTimestamp(widget.lesson.startTime),
                 Colors.purple.shade50,
                 Icons.calendar_month_outlined,
                 Colors.purple.shade400,
@@ -78,95 +93,24 @@ class _MeetDetailScreenState extends State<MeetDetailScreen> {
                 Colors.blue.shade400,
                 LessonInfoType.ders,
               ),
-              lessonsInfo(
-                'Link',
-                widget.lesson.googleMeetLink,
-                Colors.yellow.shade50,
-                Icons.link,
-                Colors.yellow.shade400,
-                LessonInfoType.link,
-              ),
+              widget.isLessonTaken
+                  ? lessonsInfo(
+                      'Link',
+                      widget.lesson.googleMeetLink,
+                      Colors.yellow.shade50,
+                      Icons.link,
+                      Colors.yellow.shade400,
+                      LessonInfoType.link,
+                    )
+                  : SizedBox.shrink(),
               SizedBox(height: 24),
-              Text(
-                'Eğitmen',
-                style: GoogleFonts.inter(
-                  color: AppColors.headTitleText,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-              SizedBox(height: 8),
-              Container(
-                height: 60,
-                decoration: BoxDecoration(
-                  color: AppColors.secondary,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Padding(
-                  padding: EdgeInsets.only(left: 12),
-                  child: Row(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Icon(
-                        Icons.person,
-                        size: 36,
-                        color: AppColors.button,
-                      ),
-                      SizedBox(width: 12),
-                      Text(
-                        widget.lesson.creator,
-                        style: GoogleFonts.inter(
-                            color: AppColors.headTitleText,
-                            fontWeight: FontWeight.bold,
-                            fontSize: 18),
-                      ),
-                    ],
-                  ),
-                ),
-              ),
+              buildSectionTitle('Eğitmen'),
+              SizedBox(height: 12),
+              buildInstructorInfo(),
               SizedBox(height: 16),
-              Text(
-                'Öğrenciler',
-                style: GoogleFonts.inter(
-                  color: AppColors.headTitleText,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 24,
-                ),
-              ),
-              SizedBox(height: 8),
-              Padding(
-                padding: const EdgeInsets.all(12.0),
-                child: ListView.builder(
-                  shrinkWrap:
-                      true, 
-                  itemCount: widget.lesson.participants.length,
-                  itemBuilder: (context, index) {
-                    return Padding(
-                      padding: const EdgeInsets.only(
-                          bottom: 8.0), 
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.person,
-                            size: 36,
-                            color: AppColors.button,
-                          ),
-                          SizedBox(width: 12),
-                          Text(
-                            widget.lesson.participants[index],
-                            style: GoogleFonts.inter(
-                              color: AppColors.headTitleText,
-                              fontWeight: FontWeight.bold,
-                              fontSize: 18,
-                            ),
-                          ),
-                        ],
-                      ),
-                    );
-                  },
-                  physics: ClampingScrollPhysics(),
-                ),
-              )
+              buildSectionTitle('Öğrenciler'),
+              SizedBox(height: 16),
+              buildParticipantsList(),
             ],
           ),
         ),
@@ -174,18 +118,24 @@ class _MeetDetailScreenState extends State<MeetDetailScreen> {
     );
   }
 
-String formatTimestamp(Timestamp timestamp) {
-  DateTime date = timestamp.toDate();
-  return DateFormat('d MMMM yyyy HH:mm').format(date); // Örnek: 14 August 2024 13:50
-}
+  String formatTimestamp(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return DateFormat('d MMMM yyyy HH:mm').format(date);
+  }
 
-String formatTime(Timestamp timestamp) {
-  DateTime date = timestamp.toDate();
-  return DateFormat('HH:mm').format(date); // Örnek: 13:50
-}
+  String formatTime(Timestamp timestamp) {
+    DateTime date = timestamp.toDate();
+    return DateFormat('HH:mm').format(date);
+  }
 
-  Padding lessonsInfo(String title, String subTitle, Color color, IconData icon,
-      Color iconColor, LessonInfoType type) {
+  Padding lessonsInfo(
+    String title,
+    String subTitle,
+    Color color,
+    IconData icon,
+    Color iconColor,
+    LessonInfoType type,
+  ) {
     return Padding(
       padding: EdgeInsets.only(top: 14),
       child: Row(
@@ -204,58 +154,158 @@ String formatTime(Timestamp timestamp) {
               ),
             ),
           ),
-          Padding(
-            padding: EdgeInsets.only(left: 24),
+          SizedBox(width: 24),
+          Expanded(
             child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-               Flexible(
-  child: Text(
-    subTitle,
-    style: GoogleFonts.inter(
-      color: AppColors.headTitleText,
-      fontWeight: FontWeight.w600,
-      fontSize: 16,
-    ),
-    overflow: TextOverflow.ellipsis,
-    maxLines: 1,
-  ),
-),
-
+                Text(
+                  title,
+                  style: GoogleFonts.inter(
+                    color: AppColors.subTitleText,
+                    fontWeight: FontWeight.w500,
+                    fontSize: 14,
+                  ),
+                ),
                 Row(
                   children: [
-                    Text(
-                      subTitle,
-                      style: GoogleFonts.inter(
-                          color: AppColors.headTitleText,
-                          fontWeight: FontWeight.w600,
-                          fontSize: 16),
+                    Flexible(
+                      child: GestureDetector(
+                        onTap: () {
+                          Clipboard.setData(ClipboardData(text: subTitle));
+                          ScaffoldMessenger.of(context).showSnackBar(
+                            SnackBar(
+                              content: Text('Meet Kopyalandı!'),
+                            ),
+                          );
+                        },
+                        child: Text(
+                          subTitle,
+                          style: GoogleFonts.inter(
+                            color: AppColors.headTitleText,
+                            fontWeight: FontWeight.w600,
+                            fontSize: 16,
+                          ),
+                          overflow: TextOverflow.ellipsis,
+                        ),
+                      ),
                     ),
                     if (type == LessonInfoType.tarih) ...[
                       SizedBox(width: 8),
                       Text(
                         '-',
                         style: GoogleFonts.inter(
-                            color: AppColors.headTitleText,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16),
+                          color: AppColors.headTitleText,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
                       ),
                       SizedBox(width: 8),
                       Text(
-                        formatTime(widget.lesson.endTime), 
+                        formatTime(widget.lesson.endTime),
                         style: GoogleFonts.inter(
-                            color: AppColors.headTitleText,
-                            fontWeight: FontWeight.w600,
-                            fontSize: 16 ),
+                          color: AppColors.headTitleText,
+                          fontWeight: FontWeight.w600,
+                          fontSize: 16,
+                        ),
                       ),
                     ],
                   ],
-                )
+                ),
               ],
             ),
-          )
+          ),
         ],
       ),
     );
+  }
+
+  Widget buildSectionTitle(String title) {
+    return Text(
+      title,
+      style: GoogleFonts.inter(
+        color: AppColors.headTitleText,
+        fontWeight: FontWeight.bold,
+        fontSize: 24,
+      ),
+    );
+  }
+
+  Widget buildInstructorInfo() {
+    return Container(
+      height: 60,
+      decoration: BoxDecoration(
+        color: AppColors.secondary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      child: Padding(
+        padding: EdgeInsets.only(left: 12),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Icon(
+              Icons.person,
+              size: 36,
+              color: AppColors.button,
+            ),
+            SizedBox(width: 12),
+            Text(
+              widget.lesson.creator,
+              style: GoogleFonts.inter(
+                color: AppColors.headTitleText,
+                fontWeight: FontWeight.bold,
+                fontSize: 18,
+              ),
+              overflow: TextOverflow.ellipsis,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  Widget buildParticipantsList() {
+    return ListView.builder(
+      shrinkWrap: true,
+      itemCount: widget.lesson.participants.length,
+      itemBuilder: (context, index) {
+        return Padding(
+          padding: const EdgeInsets.only(bottom: 12.0),
+          child: Padding(
+            padding: const EdgeInsets.only(left: 12),
+            child: Row(
+              children: [
+                Icon(
+                  Icons.person,
+                  size: 36,
+                  color: AppColors.button,
+                ),
+                SizedBox(width: 12),
+                Flexible(
+                  child: Text(
+                    widget.lesson.participants[index],
+                    style: GoogleFonts.inter(
+                      color: AppColors.headTitleText,
+                      fontWeight: FontWeight.bold,
+                      fontSize: 18,
+                    ),
+                    overflow: TextOverflow.ellipsis,
+                  ),
+                ),
+              ],
+            ),
+          ),
+        );
+      },
+      physics: ClampingScrollPhysics(),
+    );
+  }
+
+  void _launchURL(String url) async {
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
   }
 }
